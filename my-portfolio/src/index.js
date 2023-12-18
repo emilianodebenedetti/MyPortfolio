fdebaf685b5fd672a0c4b15bccf06df643045c4f
Emiliano <emilianodebenedetti@outlook.com>
2023-12-18 03:12:56 -0300

    Quito HasRouter

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';  
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,8 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>
 );
